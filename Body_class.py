665c57df49c73669f8e90c539ad11a505877bf64
Laura <laurahannah44@gmail.com>
2020-06-27 01:12:59 +0100

    New weapon and tweaks.

--- Body_class.py
+++ Body_class.py
@@ -7,7 +7,7 @@ from Vector_class import Vector
 
 
 class Body:
-    def __init__(self, P, θ=0, v=(0, 0), ω=0, m=0, r=5, friction=(0, 0), body_type=None, shot_type=None, update_type=0, threat_reqs=None, threat_to=None, damage=1, threatened_by=None, health=1, self_destruct=None, player_controls=None):
+    def __init__(self, P, θ=0, v=(0, 0), ω=0, m=0, q=0, dm=0, dq=0, r=5, friction=(0, 0), body_type=None, shot_type=None, update_type=0, threat_reqs=None, threat_to=None, damage=1, threatened_by=None, health=1, self_destruct=None, player_controls=None):
         self.defP, self.defP2 = self.P, self.P2 = Vector(*P), None
         self.defv, self.defv2 = self.v, self.v2 = Vector(*v), None
         self.defθ, self.defθ2 = self.θ, self.θ2 = θ, None
@@ -16,6 +16,9 @@ class Body:
         else:
             self.defω, self.defω2 = ω, None
         self.defm = self.m = m
+        self.defq = self.q = q
+        self.defdm = self.dm = dm
+        self.defdq = self.dq = dq
         self.defr = self.r = r
         self.defage = self.age = 0
 
@@ -34,6 +37,7 @@ class Body:
         self.dark_colour = (164, 164, 164)
 
         self.shot_type = shot_type
+        self.shot_age = 0
 
         self.bullets = list()
         if self.shot_type is not None:
@@ -41,31 +45,42 @@ class Body:
             P = self.P + self.r * facing
             v = self.v + self.player_controls[4][1] * facing
             if self.shot_type == "gun":
+                self.shot_cooldown = 30
                 for i in range(4):
-                    self.bullets.append(Body(P, θ=None, v=Vector(0.6, 0), m=1, r=5, friction=(0, 0), body_type="pellet", update_type=1,
-                                             threat_reqs={"t": 10}, threat_to=("player", "pellet", "shrapnel"), damage=2,
-                                             threatened_by=("player", "pellet", "shrapnel", "bullet", "star"), health=2, self_destruct={"t": 300, "v": 20, "s": True}))
+                    self.bullets.append(Body(P, θ=None, v=Vector(0.75, 0), m=1, r=5, friction=(1 / 2000, 0), body_type="pellet", update_type=1,
+                                             threat_reqs={"t": 10}, threat_to=("player", "pellet", "shrapnel", "bullet", "static"), damage=2,
+                                             threatened_by=("player", "pellet", "shrapnel", "bullet", "static", "star"), health=2, self_destruct={"t": 300, "v": 20, "s": True}))
             elif self.shot_type == "shotgun":
+                self.shot_cooldown = 90
                 for i in range(6):
-                    self.bullets.append(Body(P, θ=None, v=Vector(1.2, 0), m=0, r=3, friction=(1 / 400, 0), body_type="shrapnel", update_type=1,
-                                             threat_reqs={"t": 5}, threat_to=("player", "pellet"), damage=1,
-                                             threatened_by=("player", "pellet", "bullet", "star"), health=1, self_destruct={"t": 100, "v": 20, "s": True}))
+                    self.bullets.append(Body(P, θ=None, v=Vector(1, 0), r=3, friction=(1 / 400, 0), body_type="shrapnel", update_type=1,
+                                             threat_reqs={"t": 5}, threat_to=("player", "pellet", "bullet", "static"), damage=1,
+                                             threatened_by=("player", "pellet", "bullet", "static", "star"), health=1, self_destruct={"t": 100, "v": 20, "s": True}))
             elif self.shot_type == "sniper":
-                for i in range(1):
-                    self.bullets.append(Body(P, θ=None, v=Vector(1.5, 0), m=10, r=4, friction=(0, 0), body_type="bullet", update_type=1,
-                                             threat_reqs={"t": 10}, threat_to=("player", "pellet", "shrapnel", "bullet"), damage=3,
-                                             threatened_by=("player", "bullet", "star"), health=2, self_destruct={"t": 300, "v": 20, "s": True}))
+                self.shot_cooldown = 120
+                for i in range(2):
+                    self.bullets.append(Body(P, θ=None, v=Vector(1.5, 0), m=5, dm=30, r=4, friction=(0, 0), body_type="bullet", update_type=1,
+                                             threat_reqs={"t": 10}, threat_to=("player", "pellet", "shrapnel", "bullet", "static"), damage=3,
+                                             threatened_by=("player", "pellet", "shrapnel", "bullet", "static", "star"), health=2, self_destruct={"t": 300, "v": 20, "s": True}))
+            elif self.shot_type == "science":
+                self.shot_cooldown = 180
+                for i in range(7):
+                    self.bullets.append(Body(P, θ=None, v=Vector(0.75, 0), m=0.5, r=6, friction=(0, 0), body_type="static", update_type=1,
+                                             threat_reqs={"t": 20}, threat_to=("player", "pellet", "shrapnel", "bullet"), damage=2,
+                                             threatened_by=("player", "pellet", "shrapnel", "bullet", "star"), health=1, self_destruct={"t": 80, "s": True}))
 
     def default(self, parent=None):
         if self.body_type in ("pellet", "bullet"):
             facing = Vector(math.cos(parent.θ), math.sin(parent.θ))
             self.P, self.P2 = parent.P + parent.r * facing, None
-            self.v, self.v2 = parent.v + self.defv.norm() * parent.player_controls[4][1] * facing, None
-        elif self.body_type == "shrapnel":
-            θ = parent.θ + random.uniform(-math.pi / 8, math.pi / 8)
+            self.v, self.v2 = 0.5 * parent.v + self.defv.norm() * parent.player_controls[4][1] * facing, None
+        elif self.body_type in ("shrapnel", "static"):
+            Δθ = math.pi / (8, 512)[self.body_type == "static"]
+            θ = parent.θ + random.uniform(-Δθ, Δθ)
             facing = Vector(math.cos(θ), math.sin(θ))
             self.P, self.P2 = parent.P + parent.r * facing, None
-            self.v, self.v2 = random.uniform(0.5, 1.5) * (parent.v + self.defv.norm() * parent.player_controls[4][1] * facing), None
+            Δv = (0.25, 0.001)[self.body_type == "static"]
+            self.v, self.v2 = random.uniform(1 - Δv, 1 + Δv) * (parent.v + self.defv.norm() * parent.player_controls[4][1] * facing), None
         else:
             self.P = self.defP
             self.P2 = self.defP2
@@ -76,41 +91,50 @@ class Body:
         self.ω = self.defω
         self.ω2 = self.defω2
         self.m = self.defm
+        self.q = self.defq
+        self.dm = self.defdm
+        self.dq = self.defdq
         self.r = self.defr
-        self.age = self.defage
 
         self.health = self.defhealth
 
-    def update(self, dt, bodies, G, keys):
+        self.age = 0
+        self.shot_age = 0
+
+    def update(self, dt, bodies, G, Q, keys):
         if self.update_type:
-            dPbdt1, dvbdt1, dθbdt1, dωbdt1 = self.dbdt(self.P, self.v, self.θ, self.ω, bodies, G, keys)
-            dP1, dv1 = dPbdt1 * dt, dvbdt1 * dt
+            dPbdt1, dvbdt1, dθbdt1, dωbdt1, dmbdt1, ddmbdt1, dqbdt1, ddqbdt1 = self.dbdt(self.P, self.v, self.θ, self.ω, self.m, self.dm, self.q, self.dq, bodies, G, Q, keys)
+            dP1, dv1, dm1, ddm1, dq1, ddq1 = dPbdt1 * dt, dvbdt1 * dt, dmbdt1 * dt, ddmbdt1 * dt, dqbdt1 * dt, ddqbdt1 * dt
             if self.θ is not None:
                 dθ1, dω1 = dθbdt1 * dt, dωbdt1 * dt
             for i in range(self.update_type - 1):
-                dPbdt2, dvbdt2, dθbdt2, dωbdt2 = self.dbdt(self.P + dP1, self.v + dv1, self.θ + dθ1, self.ω + dω1, bodies, G, keys)
-                dP2, dv2 = dPbdt2 * dt, dvbdt2 * dt
+                dPbdt2, dvbdt2, dθbdt2, dωbdt2, dmbdt2, ddmbdt2, dqbdt2, ddqbdt2 = self.dbdt(self.P + dP1, self.v + dv1, self.θ + dθ1, self.ω + dω1, self.m + dm, self.dm + ddm, self.q + dq, self.dq + ddq, bodies, G, Q, keys)
+                dP2, dv2, dm2, ddm2, dq2, ddq2 = dPbdt2 * dt, dvbdt2 * dt, dmbdt2 * dt, ddmbdt2 * dt, dqbdt2 * dt, ddqbdt2 * dt
                 if self.θ is not None:
                     dθ2, dω2 = dθbdt2 * dt, dωbdt2 * dt
-                dP1, dv1 = (dP1 + dP2) * 0.5, (dv1 + dv2) * 0.5
+                dP1, dv1, dm1, ddm1, dq1, ddq1 = (dP1 + dP2) * 0.5, (dv1 + dv2) * 0.5, (dm1 + dm2) * 0.5, (ddm1 + ddm2) * 0.5, (dq1 + dq2) * 0.5, (ddq1 + ddq2) * 0.5
                 if self.θ is not None:
                     dθ1, dω1 = (dθ1 + dθ2) * 0.5, (dω1 + dω2) * 0.5
-            self.P2, self.v2 = self.P + dP1, self.v + dv1
+            self.P2, self.v2, self.m2, self.dm2, self.q2, self.dq2 = self.P + dP1, self.v + dv1, self.m + dm1, self.dm + ddm1, self.q + dq1, self.dq + ddq1
             if self.θ is not None:
                 self.θ2, self.ω2 = self.θ + dθ1, self.ω + dω1
 
-    def dbdt(self, P, v, θ, ω, bodies, G, keys):
+    def dbdt(self, P, v, θ, ω, m, dm, q, dq, bodies, G, Q, keys):
         dPbdt = v
-        dvbdt = self.Σa(P, v, θ, ω, bodies, G, keys)
+        dvbdt = self.Σa(P, v, θ, ω, m, dm, q, dq, bodies, G, Q, keys)
         if θ is not None:
             dθbdt = ω
-            dωbdt = self.Σα(P, v, θ, ω, bodies, G, keys)
+            dωbdt = self.Σα(P, v, θ, ω, m, dm, q, dq, bodies, G, Q, keys)
         else:
             dθbdt = None
             dωbdt = None
-        return dPbdt, dvbdt, dθbdt, dωbdt
+        dmbdt = dm
+        dqbdt = dq
+        ddmbdt = 0
+        ddqbdt = (0, 0.5)[self.body_type == "static"] + 4 * dq
+        return dPbdt, dvbdt, dθbdt, dωbdt, dmbdt, ddmbdt, dqbdt, ddqbdt
 
-    def Σa(self, P, v, θ, ω, bodies, G, keys):
+    def Σa(self, P, v, θ, ω, m, dm, q, dq, bodies, G, Q, keys):
         a = Vector(0, 0)
 
         # friction
@@ -123,7 +147,7 @@ class Body:
 
         a -= v * self.friction[0] * min_distance
 
-        # gravity
+        # gravity and electrostatic
         for body in bodies:
             if self.P != body.P:
                 s = self.P.distance_to(body.P)
@@ -132,6 +156,10 @@ class Body:
 
                 # scaled gravacc equation to account for overlap of objects
                 a += (body.P - self.P).normalize() * G * body.m * (s ** 2 + (self.r + body.r) ** 4 * s ** -2) ** -1
+
+                # elecstatacc
+                if m != 0:
+                    a += -1 * (body.P - self.P).normalize() * Q * q * body.q * s ** -2 * m ** -1
 
         # thrust
         if self.player_controls is not None:
@@ -143,7 +171,7 @@ class Body:
 
         return a
 
-    def Σα(self, P, v, θ, ω, bodies, G, keys):
+    def Σα(self, P, v, θ, ω, m, dm, q, dq, bodies, G, Q, keys):
         α = 0
 
         # friction
@@ -167,15 +195,19 @@ class Body:
             self.v, self.v2 = self.v2, None
             self.θ, self.θ2 = self.θ2, None
             self.ω, self.ω2 = self.ω2, None
+            self.m, self.m2 = self.m2, None
+            self.dm, self.dm2 = self.dm2, None
+            self.q, self.q2 = self.q2, None
+            self.dq, self.dq2 = self.dq2, None
 
             if self.self_destruct is not None:
                 if "t" in self.self_destruct and self.age >= self.self_destruct["t"]:
                     bodies.remove(self)
                     return
-                if "v" in self.self_destruct and self.v.norm() <= self.self_destruct["v"]:
+                elif "v" in self.self_destruct and self.v.norm() <= self.self_destruct["v"]:
                     bodies.remove(self)
                     return
-                if "s" in self.self_destruct and (self.P[0] < 0 or self.P[1] < 0 or self.P[0] > screen_dims[0] or self.P[1] > screen_dims[1]):
+                elif "s" in self.self_destruct and (self.P[0] < 0 or self.P[1] < 0 or self.P[0] > screen_dims[0] or self.P[1] > screen_dims[1]):
                     bodies.remove(self)
                     return
 
@@ -191,25 +223,33 @@ class Body:
                     bodies.remove(self)
                 if body.health <= 0:
                     bodies.remove(body)
+                return
 
     def fire(self, bodies):
+        self.shot_age = self.age
         if self.shot_type in ("gun", "sniper"):
             for bullet in self.bullets:
                 if bullet not in bodies:
                     bullet.default(self)
                     bodies.append(bullet)
                     break
-        elif self.shot_type == "shotgun":
+        elif self.shot_type in ("shotgun", "science"):
             for bullet in self.bullets:
                 if bullet not in bodies:
                     bullet.default(self)
                     bodies.append(bullet)
 
     def draw(self, screen):
-        pygame.draw.circle(screen, self.colour, round(self.P), self.r)
-        pygame.draw.circle(screen, self.dark_colour, round(self.P), self.r - 2)
+        if self.threat_reqs is None or self.age >= self.threat_reqs["t"]:
+            colour = (255, 200, 200)
+            dark_colour = (164, 100, 100)
+        else:
+            colour = (255, 255, 255)
+            dark_colour = (164, 164, 164)
+        pygame.draw.circle(screen, colour, round(self.P), self.r)
+        pygame.draw.circle(screen, dark_colour, round(self.P), self.r - 2)
         if self.θ is not None:
-            pygame.draw.circle(screen, self.colour, round(self.P + self.r * Vector(math.cos(self.θ), math.sin(self.θ))), round(self.r / 4))
+            pygame.draw.circle(screen, colour, round(self.P + self.r * Vector(math.cos(self.θ), math.sin(self.θ))), round(self.r / 4))
         if self.body_type == "player":
             for i in range(self.health):
-                pygame.draw.circle(screen, self.colour, round(self.defP + i * Vector(10, 0)), 3)
+                pygame.draw.circle(screen, colour, round(self.defP + i * Vector(10, 0)), 3)
--- main.py
+++ main.py
@@ -11,23 +11,25 @@ screen_dims = (1920, 1020)
 screen = pygame.display.set_mode(screen_dims, pygame.RESIZABLE)
 
 G = 30000
+Q = 30000
 
 bodies = list()
 players = list()
-bodies.append(Body((int(screen_dims[0] / 2 + 300), int(screen_dims[1] / 2)), θ=None, m=160, r=40, body_type="star", threat_to=("player", "pellet", "bullet", "shrapnel"), damage=5))
-bodies.append(Body((int(screen_dims[0] / 2 - 300), int(screen_dims[1] / 2)), θ=None, m=160, r=40, body_type="star", threat_to=("player", "pellet", "bullet", "shrapnel"), damage=5))
-player1 = Body((200, 200), m=0, r=10, friction=(1 / 400, 5), body_type="player", shot_type="shotgun",
-               update_type=1, threat_to=("pellet", "shrapnel", "bullet"), damage=5, threatened_by=("pellet", "shrapnel", "bullet", "star"), health=5, self_destruct={"s": True},
+bodies.append(Body((int(screen_dims[0] / 2 + 300), int(screen_dims[1] / 2)), θ=None, m=160, r=40, body_type="star", threat_to=("player", "pellet", "shrapnel", "bullet", "static"), damage=5))
+bodies.append(Body((int(screen_dims[0] / 2 - 300), int(screen_dims[1] / 2)), θ=None, m=160, r=40, body_type="star", threat_to=("player", "pellet", "shrapnel", "bullet", "static"), damage=5))
+player1 = Body((200, 200), r=10, friction=(1 / 400, 5), body_type="player", shot_type="shotgun",
+               update_type=1, threat_to=("pellet", "shrapnel", "bullet", "static"), damage=5, threatened_by=("pellet", "shrapnel", "bullet", "static", "star"), health=5, self_destruct={"s": True},
                player_controls=((pygame.K_w, 250), (pygame.K_a, -30), (pygame.K_s, -50), (pygame.K_d, 30), (pygame.K_LSHIFT, 300), pygame.K_r))
-player2 = Body((screen_dims[0] - 200, screen_dims[1] - 200), m=0, r=10, friction=(1 / 400, 5), body_type="player", shot_type="gun",
-               update_type=1, threat_to=("pellet", "shrapnel", "bullet"), damage=5, threatened_by=("pellet", "shrapnel", "bullet", "star"), health=5, self_destruct={"s": True},
+player2 = Body((screen_dims[0] - 200, screen_dims[1] - 200), r=10, friction=(1 / 400, 5), body_type="player", shot_type="gun",
+               update_type=1, threat_to=("pellet", "shrapnel", "bullet", "static"), damage=5, threatened_by=("pellet", "shrapnel", "bullet", "static", "star"), health=5, self_destruct={"s": True},
                player_controls=((pygame.K_UP, 250), (pygame.K_LEFT, -30), (pygame.K_DOWN, 50), (pygame.K_RIGHT, 30), (pygame.K_RCTRL, 300), pygame.K_RETURN))
-player3 = Body((screen_dims[0] - 200, 200), m=0, r=10, friction=(1 / 400, 5), body_type="player", shot_type="sniper",
-               update_type=1, threat_to=("pellet", "shrapnel", "bullet"), damage=5, threatened_by=("pellet", "shrapnel", "bullet", "star"), health=5, self_destruct={"s": True},
+player3 = Body((screen_dims[0] - 200, 200), r=10, friction=(1 / 400, 5), body_type="player", shot_type="sniper",
+               update_type=1, threat_to=("pellet", "shrapnel", "bullet", "static"), damage=5, threatened_by=("pellet", "shrapnel", "bullet", "static", "star"), health=5, self_destruct={"s": True},
                player_controls=((pygame.K_i, 250), (pygame.K_j, -30), (pygame.K_k, 50), (pygame.K_l, 30), (pygame.K_n, 300), pygame.K_p))
-player4 = Body((200, screen_dims[1] - 200), m=0, r=10, friction=(1 / 400, 5), body_type="player", shot_type="shotgun",
-               update_type=1, threat_to=("pellet", "shrapnel", "bullet"), damage=5, threatened_by=("pellet", "shrapnel", "bullet", "star"), health=5, self_destruct={"s": True},
+player4 = Body((200, screen_dims[1] - 200), r=10, friction=(1 / 400, 5), body_type="player", shot_type="science",
+               update_type=1, threat_to=("pellet", "shrapnel", "bullet", "static"), damage=5, threatened_by=("pellet", "shrapnel", "bullet", "static", "star"), health=5, self_destruct={"s": True},
                player_controls=((pygame.K_t, 250), (pygame.K_f, -30), (pygame.K_g, 50), (pygame.K_h, 30), (pygame.K_c, 300), pygame.K_u))
+
 players.append(player1)
 players.append(player2)
 players.append(player3)
@@ -48,18 +50,17 @@ while not done:
             screen_dims = (event.w, event.h)
             screen = pygame.display.set_mode(screen_dims, pygame.RESIZABLE)
 
-        if event.type == pygame.KEYDOWN:
-            for player in players:
-                if player in bodies:
-                    if event.key == player.player_controls[4][0]:
-                        player.fire(bodies)
-                else:
-                    if event.key == player.player_controls[5]:
-                        player.default()
-                        bodies.append(player)
+    for player in players:
+        if player in bodies:
+            if keys[player.player_controls[4][0]] and (player.age - player.shot_age) >= player.shot_cooldown:
+                player.fire(bodies)
+        else:
+            if keys[player.player_controls[5]]:
+                player.default()
+                bodies.append(player)
 
     for body in bodies:
-        body.update(1 / FPS, bodies, G, keys)
+        body.update(1 / FPS, bodies, G, Q, keys)
     for body in bodies:
         body.step(bodies, screen_dims)
 
